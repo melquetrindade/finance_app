b95879a469f7d7b173c8c1d0f2f60f7caccfbbb1
melquetrindade <melquetrindade654@gmail.com>
2024-11-23 19:47:12 -0300

    refatorando o primary botton

--- finance_app/lib/widgets/primary_button.dart
+++ finance_app/lib/widgets/primary_button.dart
@@ -4,40 +4,31 @@ import 'package:flutter/material.dart';
 class PrimaryButton extends StatelessWidget {
   final VoidCallback? onPressed;
   final String text;
+  final BorderRadius _borderRadius = BorderRadius.circular(24);
 
-  const PrimaryButton({
-    super.key,
-    this.onPressed,
-    required this.text,
-  });
+  PrimaryButton(
+      {super.key, this.onPressed, required this.text});
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      borderRadius: BorderRadius.circular(38),
-      child: Ink(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(38),
-            gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [AppColors.greelightOne, AppColors.greelightTwo])),
-        child: InkWell(
-          borderRadius: BorderRadius.circular(38),
-          onTap: onPressed,
-          child: Container(
-            alignment: Alignment.center,
-            height: 64.0,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(38),
-            ),
-            child: Text(
-              text,
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600),
-            ),
+    return Ink(
+      height: 48.0,
+      decoration: BoxDecoration(
+          borderRadius: _borderRadius,
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [AppColors.greelightOne, AppColors.greelightTwo])),
+      child: InkWell(
+        borderRadius: _borderRadius,
+        onTap: onPressed,
+        child: Align(
+          child: Text(
+            text,
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.w600),
           ),
         ),
       ),
